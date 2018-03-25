26128e0ed92be1d1a9adcd223f4a823d9b437d76
Sylvain Legault <sylvain.legault@pj.ca>
2018-03-25 07:33:21 -0400

    Initial setup with webpack, storybook, jest

---
+++ react-demo-old/README.md
@@ -0,0 +1,8 @@
+# react-demo
+ReactJS Demo App with webpack, redux, redux-saga
+
+
+# 3rd Party
+https://webpack.js.org/
+https://storybook.js.org/
+https://facebook.github.io/jest/
---
+++ react-demo-old/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const title = 'Setup completed';
+    return (
+        <div>{title}</div>   
+    );
+  }
+}
+
+export default App;
